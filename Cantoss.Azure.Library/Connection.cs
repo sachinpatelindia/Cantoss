c263ca85f9666e2592569dd4ef994ade3113e142
Sachin Patel <sachin@cantoss.com>
2024-08-12 22:19:42 +0530

    nuget package updated

---
+++ Cantoss.Azure.Library/Connection.cs
@@ -0,0 +1,13 @@
+﻿namespace Cantoss.Azure.Library
+{
+    public class Connection
+    {
+        public CosmosDb? CosmosDb { get; set; }
+    }
+
+    public class CosmosDb
+    {
+        public required string EndpointUri { get; set; }
+        public required string PrimaryKey { get; set; }
+    }
+}
